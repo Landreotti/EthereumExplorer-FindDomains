05bcdbaaae70b687d77c1d486467740e71c4cba5
= <=>
2022-06-13 19:08:39 -0300

    Add Methods HTTP

--- src/index.js
+++ src/index.js
@@ -2,11 +2,32 @@ const express = require('express')
 
 const app = express();
 
-app.get("/domains", (request, response) => {
+app.get("/domain", (request, response) => {
     return response.json(
-        ["domain: iam.alice.eth", "label:  iam", "label hash: keccak256(‘eth’) = 0x4f5b812789fc606be1b3b16908db13fc7a9adf7ca72641f84d75b47069d3d7f0", "node: The output of the namehash function, used to uniquely identify a name in EN",
-        "domains(where: {name:'vitalik.eth'}) {id,name, label/Namelabel, hash"]
+        ["domains{id,name, label/Namelabel, hash}"]
     )
+})
+
+app.post("/domain", (request, response) => {
+    return response.json(
+        ["domains{id,name, label/Namelabel, hash}"]
+    )
+})
+
+app.post("/domain/:id", (request, response) => {
+    return response.json(
+        ["domains{id,name, label/Namelabel, hash}"]
+    )
+})
+
+app.patch("domain/:id", (request, response) => {
+    return response.json(
+        ["domains{id,name, label/Namelabel, hash}"]
+    )
+})
+
+app.delete("/domain/:id", (request, response) => {
+    ["domains{id,name, label/Namelabel, hash"]
 })
 
 //localhost:3333
