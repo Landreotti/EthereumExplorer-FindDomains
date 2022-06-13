d3b9b776920f6e88efd1d4ae5271376edb2849bc
= <=>
2022-06-13 19:56:59 -0300

    Change domains json

--- src/index.js
+++ src/index.js
@@ -4,31 +4,31 @@ const app = express();
 
 app.get("/domain", (request, response) => {
     return response.json(
-        ["domains{id,name, label/Namelabel, hash}"]
+        ["domains {id,name, label/Namelabel, hash}"]
     )
 })
 
 app.post("/domain", (request, response) => {
     return response.json(
-        ["domains{id,name, label/Namelabel, hash}"]
+        ["domains {id,name, label/Namelabel, hash}"]
     )
 })
 
-app.post("/domain/:id", (request, response) => {
+app.put("/domain/:id", (request, response) => {
     return response.json(
-        ["domains{id,name, label/Namelabel, hash}"]
+        ["domains {id,name, label/Namelabel, hash}", "domains {id,name, label/Namelabel, hash}", "domains {id,name, label/Namelabel, hash}", "domains {id,name, label/Namelabel, hash}"]
     )
 })
 
-app.patch("domain/:id", (request, response) => {
+
+app.patch("/domain/:id", (request, response) => {
     return response.json(
-        ["domains{id,name, label/Namelabel, hash}"]
+        ["domains {id,name, label/Namelabel, hash}"]
     )
 })
 
 app.delete("/domain/:id", (request, response) => {
-    ["domains{id,name, label/Namelabel, hash"]
+    ["domains {id,name, label/Namelabel, hash"]
 })
 
-//localhost:3333
 app.listen(3333);
